3f2f6d51ad74d7b4b0cd92d36448363848dc6f96
lilo22ga <lilo22ga@student.ju.se>
2023-04-04 13:14:54 +0200

    set up javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+const mondayElement = querySelector(".monday");
+const tuesdayElement = querySelecetor(".tuesday");
+const wednesdayElement = querySelecetor(".wednesday");
+const thursdayElement = querySelecetor(".thursday");
+const fridayElement = querySelecetor(".friday");
+const addElement = document.getElementById("addToList");
+
+let toDos = [];
+
+function addToDos() {}
