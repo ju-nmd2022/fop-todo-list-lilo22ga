5bc2121ba4e4f22025fad2fe26d65b2e4a20403a
lilo22ga <lilo22ga@student.ju.se>
2023-04-06 17:39:20 +0200

    add to do's and icons

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-const mondayElement = querySelector(".monday");
-const tuesdayElement = querySelecetor(".tuesday");
-const wednesdayElement = querySelecetor(".wednesday");
-const thursdayElement = querySelecetor(".thursday");
-const fridayElement = querySelecetor(".friday");
+//create array for weekdays later?
+
+const mondayElement = document.querySelector(".monday");
+const tuesdayElement = document.querySelector(".tuesday");
+const wednesdayElement = document.querySelector(".wednesday");
+const thursdayElement = document.querySelector(".thursday");
+const fridayElement = document.querySelector(".friday");
 const addElement = document.getElementById("addToList");
+const addButton = document.getElementById("addBtn");
+const toDoMonday = document.querySelector(".toDoItem");
+const toDoElements = document.querySelector(".toDoItems");
 
 let toDos = [];
 
-function addToDos() {}
+const addIcons = (parentDiv) => {
+  const iconsDiv = document.createElement("div");
+  iconsDiv.classList.add("icons");
+
+  const checkmark = document.createElement("img");
+  checkmark.src = "checkmark2.png";
+  checkmark.alt = "checkmark";
+  checkmark.classList.add("checkmark");
+
+  const deletemark = document.createElement("img");
+  deletemark.src = "delete2.png";
+  deletemark.alt = "deletemark";
+  deletemark.classList.add("deletemark");
+
+  iconsDiv.appendChild(checkmark);
+  iconsDiv.appendChild(deletemark);
+  parentDiv.appendChild(iconsDiv);
+};
+
+// function updateList ()
+const updateList = () => {
+  toDoElements.innerHTML = "";
+  for (let toDo of toDos) {
+    const parentDiv = document.createElement("div");
+    parentDiv.classList.add("toDoItem");
+
+    const newToDo = document.createElement("div");
+    newToDo.innerHTML = toDo;
+
+    parentDiv.appendChild(newToDo);
+    addIcons(parentDiv);
+    toDoElements.appendChild(parentDiv);
+  }
+};
+
+const addToDos = () => {
+  if (addElement.value.length > null) {
+    const toDo = addElement.value;
+    toDos.push(toDo);
+    console.log(toDo);
+    updateList();
+  }
+};
+
+addButton.addEventListener("click", addToDos);
