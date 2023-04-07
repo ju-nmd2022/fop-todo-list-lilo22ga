d71a5a3f7370bc09d793184a266389cd9ad89ed9
lilo22ga <lilo22ga@student.ju.se>
2023-04-07 19:30:23 +0200

    mark items as completed and remove

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const toDoElements = document.querySelector(".toDoItems");
 
 let toDos = [];
 
+//3
 const addIcons = (parentDiv) => {
   const iconsDiv = document.createElement("div");
   iconsDiv.classList.add("icons");
@@ -29,9 +30,12 @@ const addIcons = (parentDiv) => {
   iconsDiv.appendChild(checkmark);
   iconsDiv.appendChild(deletemark);
   parentDiv.appendChild(iconsDiv);
+
+  checkmark.addEventListener("click", markAsDone);
+  deletemark.addEventListener("click", removeFromList);
 };
 
-// function updateList ()
+//2
 const updateList = () => {
   toDoElements.innerHTML = "";
   for (let toDo of toDos) {
@@ -47,6 +51,7 @@ const updateList = () => {
   }
 };
 
+//1
 const addToDos = () => {
   if (addElement.value.length > null) {
     const toDo = addElement.value;
@@ -54,6 +59,27 @@ const addToDos = () => {
     console.log(toDo);
     updateList();
   }
+};
+
+const markAsDone = (event) => {
+  const checkmark = event.target;
+  const toDoItem = checkmark.parentElement.parentElement;
+  if (toDoItem) {
+    toDoItem.style.textDecoration = "line-through";
+  }
+};
+
+const removeFromList = (event) => {
+  const deletemark = event.target;
+  const toDoItem = deletemark.parentElement.parentElement;
+  if (toDoItem) {
+    toDoItem.remove();
+  }
+
+  //doesn't work properly??
+  const listIndex = toDos.indexOf(toDoItem.innerHTML);
+  toDos.splice(listIndex, 1);
+  updateList();
 };
 
 addButton.addEventListener("click", addToDos);
